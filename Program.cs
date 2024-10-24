69b6b9f63077bedfdf6857d9e87befb822014fd6
verenstechworld <kenolyoveren@gmail.com>
2024-10-24 20:55:53 +0200

    vtwmidlab

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace VTWMidlab
+{
+    class Program
+    {
+         static void Main(string[] args)
+         {
+            string color, pluralNoun, superstar;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a Plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter the name of your favorite superstar: ");
+            superstar = Console.ReadLine(); 
+             
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I Love " + superstar);
+
+            Console.ReadLine();
+         }
+    }
+}
